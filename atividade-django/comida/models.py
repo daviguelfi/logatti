4e478ffd47bde070cfdae948834b9c6fed387e85
daviguelfi <daviguelfi@gmail.com>
2020-06-10 19:45:20 -0300

    ajust for the exercise work

--- atividade-django/comida/models.py
+++ atividade-django/comida/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Comida(models.Model):
-    id = models.AutoField(primary_key=True)
+    # id = models.AutoField(primary_key=True)
     tipoComida = models.CharField(max_length=100)
     descricao = models.CharField(max_length=200)
     quantidade = models.BigIntegerField()
